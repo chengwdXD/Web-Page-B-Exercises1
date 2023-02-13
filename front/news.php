0cfb2b0c3d6918578bf284be9c657beb64a84f9d
chengwdXD <4a339047@stust.edu.tw>
2023-02-13 21:27:18 +0800

    front資料夾裡的3個php 在marqee裡新增AD資料表的foreach

--- front/news.php
+++ front/news.php
@@ -1,8 +1,16 @@
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
 	<marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+		<?php
+		$ads = $AD->all(['sh' => 1]);
+		foreach ($ads as $ad) {
+			echo $ad['text'];
+			echo "&nbsp;&nbsp;&nbsp;";
+		}
+		?>
 	</marquee>
 	<div style="height:32px; display:block;"></div>
 	<!--正中央-->
+	<h3 class="cent">更多最新消息</h3>
 	<div style="text-align:center;">
 		<a class="bl" style="font-size:30px;" href="?do=meg&p=0">&lt;&nbsp;</a>
 		<a class="bl" style="font-size:30px;" href="?do=meg&p=0">&nbsp;&gt;</a>
