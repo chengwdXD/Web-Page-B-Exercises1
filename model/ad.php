f68b9522b156930354a31f37579bef80de365011
weide <4a339047@stust.edu.tw>
2023-02-21 11:45:00 +0800

    在model裡複製title.php改名成ad.php 刪掉第一個tr 修改應改的文字內容

---
+++ model/ad.php
@@ -0,0 +1,17 @@
+<h3 class="cent">新增動態文字廣告</h3>
+
+<form action="./api/add.php" method="post" enctype="multipart/form-data">
+<hr>
+<table>
+  
+    <tr>
+        <td style="text-align: right;">動態文字廣告:</td>
+        <td><input type="text" name="text"></td>
+    </tr>
+</table>
+<div class="cent">
+    <input type="hidden" name="table" value="Title">
+<input type="submit" value="新增">
+<input type="reset" value="重製">
+</div>
+</form>
