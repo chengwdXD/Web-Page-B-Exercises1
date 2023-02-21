d536605fe39fc7dcbb85127233ac46c5fb617c24
weide <4a339047@stust.edu.tw>
2023-02-21 11:13:11 +0800

    用ad.php複製改名成news.php 一樣修改應改欄位 在第一個td裡新增一個textarea 將input刪掉

---
+++ back/news.php
@@ -0,0 +1,54 @@
+<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
+
+    <p class="t cent botli">最新消息資料管理</p>
+    <form method="post" action="./api/edit.php">
+        <table width="100%">
+            <tbody>
+                <tr class="yel">
+       
+                    <td width="68%">最新消息</td>
+                    <td width="7%">顯示</td>
+                    <td width="7%">刪除</td>
+            
+                </tr>
+                <?php
+                $rows = $Title->all();
+                foreach ($rows as $row) {
+                    $checked = ($row['sh'] == 1) ? "checked" : "";
+
+                ?>
+                    <tr>
+                        
+                        <td width="23%">
+                            <textarea name="text[]" style="width:95%;height:62px" ><?=$row['text'];?></textarea>
+                            
+                        </td>
+                        <td width="7%">
+                            <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=$checked;?>>
+                        </td>
+                        <td width="7%">
+                            <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
+                        </td>
+                        <td>
+                           
+                            <input type="hidden" name="id[]" value="<?=$row['id'];?>">
+                        </td>
+                    </tr>
+                <?php
+                }
+                ?>
+            </tbody>
+        </table>
+        <table style="margin-top:40px; width:70%;">
+            <tbody>
+                <tr>
+                    <td width="200px"><input type="button" onclick="op(&#39;#cover&#39;,&#39;#cvr&#39;,&#39;model/title.php&#39;)" value="新增最新消息"></td>
+                    <td class="cent">
+                        <input type="hidden" name="table" value="Title">
+                        <input type="submit" value="修改確定">
+                        <input type="reset" value="重置"></td>
+                </tr>
+            </tbody>
+        </table>
+    </form>
+</div>
