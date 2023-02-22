e34cc8c130450aca06e4c02594f3d88b075caef2
chengwdXD <4a339047@stust.edu.tw>
2023-02-22 22:33:11 +0800

    新增php用foreach抓Menu的table裡面的id 補上value抓值

--- model/submenu.php
+++ model/submenu.php
@@ -9,14 +9,22 @@
         <td>次選單連結網址:</td>
         <td>刪除:</td>
     </tr>
+    <?php
+    $rows=$Menu->all(['parent'=>$_GET['id']]);
+    foreach($rows as $row){
+    ?>
     <tr>
-        <td><input type="text" name="name[]" ></td>
-        <td><input type="text" name="href[]"></td>
-        <td><input type="checkbox" name="del[]"></td>
+        <td><input type="text" name="name[]" value="<?=$row['name'];?>"></td>
+        <td><input type="text" name="href[]"value="<?=$row['href'];?>" ></td>
+        <td><input type="checkbox" name="del[]" value="<?=$row['id'];?>"></td>
+        <td><input type="hidden" name="id[]" value="<?=$row['id'];?>"></td>
     </tr>
-   
+    <?php
+    }
+    ?>
 </table>
 <div class="cent">
+    <input type="hidden" name="parent" value="<?=$_GET['id'];?>">
     <input type="hidden" name="table" value="Menu">
 <input type="submit" value="修改確定">
 <input type="reset" value="重製">
