ad35e089cd9eef6a48cda717dd6190c249b1d4f8
weide <4a339047@stust.edu.tw>
2023-02-22 10:26:49 +0800

    用model裡的ad.php複製成news.php 將td裡的input刪掉 新增textarea

---
+++ model/news.php
@@ -0,0 +1,17 @@
+<h3 class="cent">新增新增最新消息</h3>
+
+<form action="./api/add.php" method="post" enctype="multipart/form-data">
+<hr>
+<table>
+  
+    <tr>
+        <td style="text-align: right;">最新消息資料:</td>
+        <td><textarea name="text" style="width:300px;height:145px"></textarea></td>
+    </tr>
+</table>
+<div class="cent">
+    <input type="hidden" name="table" value="Title">
+<input type="submit" value="新增">
+<input type="reset" value="重製">
+</div>
+</form>
