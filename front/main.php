a88567b9185e92e3cef149907018b0df6f032d88
chengwdXD <4a339047@stust.edu.tw>
2023-02-23 23:46:52 +0800

    前台最新消息功能完成

--- front/main.php
+++ front/main.php
@@ -1,13 +1,13 @@
 <div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
     <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
-    <?php
-    $ads=$Ad->all(['sh'=>1]);
-    
-        foreach($ads as $ad){
-          echo $ad['text'];
-            echo"&nbsp;&nbsp;&nbsp;"; 
+        <?php
+        $ads = $Ad->all(['sh' => 1]);
+
+        foreach ($ads as $ad) {
+            echo $ad['text'];
+            echo "&nbsp;&nbsp;&nbsp;";
         };
-      ?> 
+        ?>
     </marquee>
     <div style="height:32px; display:block;"></div>
     <!--正中央-->
@@ -19,11 +19,11 @@
     <script>
         var lin = new Array();
         <?php
-							$mvs=$Mvim->all(['sh'=>1]);
-							foreach($mvs as $mv){
-								echo "lin.push('./upload/{$mv['img']}');";
-							}
-						?>
+        $mvs = $Mvim->all(['sh' => 1]);
+        foreach ($mvs as $mv) {
+            echo "lin.push('./upload/{$mv['img']}');";
+        }
+        ?>
         var now = 0;
         ww()
         if (lin.length > 1) {
@@ -41,8 +41,27 @@
     </script>
     <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
         <span class="t botli">最新消息區
+            <?php
+            if ($News->count(['sh' => 1]) > 5) {
+                echo "<a href = '?do=news' style='float:right'>";
+                echo "More...";
+                echo "</a>";
+            }
+            ?>
         </span>
         <ul class="ssaa" style="list-style-type:decimal;">
+            <?php
+            $news = $News->all(['sh' => 1], " limit 5");
+            foreach ($news as $n) {
+                echo "<li>";
+                echo mb_substr($n['text'], 0, 25) . "...";
+                echo "<span class='all' style='display:none'>";
+                echo $n['text'];
+                echo "</span>";
+                echo "</li>";
+            }
+
+            ?>
         </ul>
         <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
         <script>
--- test.php
+++ test.php
@@ -1,63 +1,92 @@
-<div style="width:99%; height:87%; margin:auto; overflow:auto; border:#666 1px solid;">
-    <p class="t cent botli">選單管理</p>
-    <!-- <form method="post" target="back" action="?do=tii"> -->
-    <form method="post" action="./api/edit.php">
-        <table width="100%">
-            <tbody>
-                <tr class="yel">
-                    <td width="30%">主選單名稱</td>
-                    <td width="30%">選單連結網址</td>
-                    <td width="10%">次選單數</td>
-                    <td width="10%">顯示</td>
-                    <td width="10%">刪除</td>
-                    <td></td>
-                </tr>
-                <?php
-                    $rows=$Menu->all(["parent"=>0]);
-                  foreach($rows as $row){
-                    $checked=($row['sh']==1)?"checked":"";
-                ?>
-                <tr>
-                    <td>
-                        <input type="text" name="name[]" value="<?=$row['name'];?>" style="width:95%">
-                    </td>
-                    <td>
-                        <input type="text" name="href[]" value="<?=$row['href'];?>" style="width:95%">
-                    </td>
-                    <td><?=$Menu->count(['parent'=>$row['id']]);?></td>
-                    <td>
-                        <input type="checkbox" name="sh[]" value="<?=$row['id'];?>" <?=$checked;?>>
-                    </td>
-                    <td>
-                        <input type="checkbox" name="del[]" value="<?=$row['id'];?>">
-                    </td>
-                    <td>
-                        <input type="button" 
-                              value="編輯次選單"
-                            onclick="op('#cover','#cvr','./modal/submenu.php?id=<?=$row['id'];?>')">
-                    </td>
-                        <input type="hidden" name="id[]" value="<?=$row['id'];?>">
-                </tr>
-                <?php
-                    }
-                ?>
-            </tbody>
-        </table>
-        <table style="margin-top:40px; width:70%;">
-            <tbody>
-                <tr>
-                    <td width="200px">
-                        <input type="button" 
-                            onclick="op('#cover','#cvr','./modal/menu.php')" 
-                              value="新增主選單">
-                    </td>
-                    <td class="cent">
-                        <input type="hidden" name="table" value="Menu">
-                        <input type="submit" value="修改確定"><input type="reset" value="重置">
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+<div class="di" style="height:540px; border:#999 1px solid; width:53.2%; margin:2px 0px 0px 0px; float:left; position:relative; left:20px;">
+    <marquee scrolldelay="120" direction="left" style="position:absolute; width:100%; height:40px;">
+        <?php
+        $ads = $Ad->all(['sh' => 1]);
+        foreach ($ads as $ad) {
+            echo $ad['text'];
+            echo "&nbsp;&nbsp;&nbsp;";
+        }
+            //echo q("SELECT *,GROUP_CONCAT('---',`text`) as 'ad' FROM `ad` where sh=1 group by sh;")[0]['ad'];
+        ; ?>
+    </marquee>
+    <div style="height:32px; display:block;"></div>
+    <!--正中央-->
 
-    </form>
+    <div style="width:100%; padding:2px; height:290px;">
+        <div id="mwww" loop="true" style="width:100%; height:100%;">
+
+            <div style="width:99%; height:100%; position:relative;" class="cent">
+
+                沒有資料
+            </div>
+
+        </div>
+    </div>
+    <script>
+        var lin = new Array();
+        <?php
+        $mvs = $Mvim->all(['sh' => 1]);
+        foreach ($mvs as $mv) {
+            echo "lin.push('./upload/{$mv['img']}');";
+        }
+        ?>
+        var now = 0;
+
+        ww()
+
+        if (lin.length > 1) {
+            setInterval("ww()", 3000);
+            now = 1;
+        }
+
+        function ww() {
+            $("#mwww").html("<embed loop=true src='" + lin[now] + "' style='width:99%; height:100%;'></embed>")
+            //$("#mwww").attr("src",lin[now])
+            now++;
+            if (now >= lin.length)
+                now = 0;
+        }
+    </script>
+
+
+    <div style="width:95%; padding:2px; height:190px; margin-top:10px; padding:5px 10px 5px 10px; border:#0C3 dashed 3px; position:relative;">
+        <span class="t botli">最新消息區
+            <?php
+            if ($News->count(['sh' => 1]) > 5) {
+                echo "<a href='?do=news' style='float:right'>";
+                echo "More...";
+                echo "</a>";
+            }
+
+            ?>
+        </span>
+        <ul class="ssaa" style="list-style-type:decimal;">
+            <?php
+            $news = $News->all(['sh' => 1], " limit 5");
+            foreach ($news as $n) {
+                echo "<li>";
+                echo mb_substr($n['text'], 0, 25) . "...";
+                echo "<span class='all' style='display:none'>";
+                echo $n['text'];
+                echo "</span>";
+                echo "</li>";
+            }
+
+            ?>
+        </ul>
+        <div id="altt" style="position: absolute; width: 350px; min-height: 100px; background-color: rgb(255, 255, 204); top: 50px; left: 130px; z-index: 99; display: none; padding: 5px; border: 3px double rgb(255, 153, 0); background-position: initial initial; background-repeat: initial initial;"></div>
+        <script>
+            $(".ssaa li").hover(
+                function() {
+                    $("#altt").html("<pre>" + $(this).children(".all").html() + "</pre>")
+                    $("#altt").show()
+                }
+            )
+            $(".ssaa li").mouseout(
+                function() {
+                    $("#altt").hide()
+                }
+            )
+        </script>
+    </div>
 </div>
