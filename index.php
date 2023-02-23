93bbd1cc6a486c7a519cbeb5851322d97d78290f
chengwdXD <4a339047@stust.edu.tw>
2023-02-23 23:57:53 +0800

    先新增2個div 放入up 和dn的圖片 和分別的onclick

--- index.php
+++ index.php
@@ -50,6 +50,15 @@ if(file_exists($file)){
 				<button style="width:100%; margin-left:auto; margin-right:auto; margin-top:2px; height:50px;" onclick="lo(&#39;?do=login&#39;)">管理登入</button>
 				<div style="width:89%; height:480px;" class="dbor">
 					<span class="t botli">校園映象區</span>
+					<div class="cent" onclick="pp(1)">
+<img src="./icon/up.jpg" alt="">
+					</div>
+					<div>
+
+					</div>
+					<div class="cent" onclick="pp(2)">
+					<img src="./icon/dn.jpg" alt="">
+					</div>
 					<script>
 						var nowpage = 0,
 							num = 0;
